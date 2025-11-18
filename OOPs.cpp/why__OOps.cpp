bda2e8a69936d18f3b87b569ee03ec867da0067c
Kapil_Kapil <kapiljlpt@gmail.com>
2025-11-19 01:32:42 +0530

    Update why__OOps.cpp

--- OOPs.cpp/why__OOps.cpp
+++ OOPs.cpp/why__OOps.cpp
@@ -4,13 +4,14 @@ Object-oriented programming (OOP) is defined as a programming model (and not a s
  OOP focuses on the objects that developers want to manipulate rather than the logic required to manipulate them.
 
 #Why OOps-
-#1.Reusability-> OOPs allow programmers to reuse code agin and again and extend the use of existing classes.
+#1.Reusability-> Through inheritance, OOPs allow programmers to reuse code agin and again and extend the use of existing classes.
 #2.Enhanced Security -> With encapsulation, critical data is hidden from outside interference, allowing only specific parts of the program 
 to interact with the internal states of an object, improving security and reducing unintended side effects.
 #3.Real world Mapping -> OOP mimics real world entities and have relationships by using objects,
 For instance, an "Animal" class can have different derived classes like "Dog" and "Cat," having specific behaviors 
 but following a general pattern.
 #4.Abstraction -> OOPs hide complex implementation(un-necessary ) details  from user and shows only necessary details.
+
 
 #Difference between OOps and Traditional(procedural) Programming-
 #1.
@@ -21,7 +22,7 @@ Reusability is achieved through functions and procedures, but there is no inheri
 
 #2.
 OOP:
-Focuses on objects that represent real-world entities.
+Focuses on objects that represent(mimics) real-world entities.
 Example: In a banking system, you may have objects like Account, Customer, each encapsulating its own data and functions.
 Procedural Programming:
 Focuses on procedures or functions that operate on data.
